ede82f4de1fdfc08ce0fac579b6757b79566076e
Sachin Handa <sachinhanda8349@gmail.com>
2026-06-30 21:47:21 +0530

    Time: 68 ms (61.58%), Space: 68.5 MB (51.57%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,5 +4,14 @@
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-    return nums.sort((a, b) => b - a)[k - 1];
+    let pq = new MinPriorityQueue();
+
+    for (let i = 0; i < nums.length; i++) {
+        pq.enqueue(nums[i]);
+        if (pq.size() > k) {
+            pq.dequeue();
+        }
+    }
+
+    return pq.front();
 };
