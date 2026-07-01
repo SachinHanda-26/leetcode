3f8a732b6b9b047be4c2fafd21b4da638d7706b9
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-01 16:13:51 +0530

    Time: 6 ms (38.38%), Space: 56.5 MB (17.9%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,11 +3,7 @@
  * @return {number}
  */
 var lastStoneWeight = function (stones) {
-    let pq = new MaxPriorityQueue();
-
-    for (let i = 0; i < stones.length; i++) {
-        pq.enqueue(stones[i]);
-    }
+    let pq = MaxPriorityQueue.fromArray(stones);
 
     while (pq.size() > 1) {
         let y = pq.dequeue();
