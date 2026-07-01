3a3c55f66d32cf13753ff2ff44e47bb872e1fc85
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-01 19:43:15 +0530

    Time: 30 ms (9.07%), Space: 62.6 MB (26.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,6 +4,7 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
+    // create a map of frequency - O(n)
     let map = {};
 
     for (let i = 0; i < nums.length; i++) {
@@ -13,17 +14,19 @@ var topKFrequent = function (nums, k) {
         ++map[nums[i]]
     }
 
+    //Add elements to MinPQ and restrict size to k. - O(n log k)
     let pq = new MinPriorityQueue(x => x.freq);
 
     for (key in map) {
 
-        pq.enqueue({ val: key, freq: map[key] });
+        pq.enqueue({ val: key, freq: map[key] }); // log k
 
         if (pq.size() > k) {
-            pq.dequeue();
+            pq.dequeue(); // log k
         }
 
     }
 
+    // return remaining k elements in PQ
     return pq.toArray().map(x => Number(x.val));
 };
