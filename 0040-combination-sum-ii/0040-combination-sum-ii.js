d564e9911571aa1fc043c37dab988a40fd4c089d
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-06 13:48:17 +0530

    Time: 1 ms (97.17%), Space: 56.9 MB (57.7%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function (arr, target) {
+    let result = [];
+    arr.sort((a, b) => a - b);
+
+    function backtrack(remainingSum, path, start) {
+
+        if (remainingSum === 0) {
+            result.push([...path]);
+        }
+
+        if (remainingSum <= 0) {
+            return;
+        }
+
+        for (let i = start; i < arr.length; i++) {
+            if(i > start && arr[i - 1] === arr[i]){
+                continue;
+            }
+            path.push(arr[i]);
+            backtrack(remainingSum - arr[i], path, i + 1);
+            path.pop();
+        }
+    }
+
+    backtrack(target, [], 0);
+
+    return result;
+
+};
