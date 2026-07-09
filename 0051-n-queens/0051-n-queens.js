25f3fc5664eaff50df2f27ba0b99912767795d1e
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-09 19:19:10 +0530

    Time: 10 ms (73.8%), Space: 59.3 MB (35.39%) - LeetHub

--- 0051-n-queens/0051-n-queens.js
+++ 0051-n-queens/0051-n-queens.js
@@ -49,6 +49,7 @@ var solveNQueens = function (n) {
 
 function transform(board) {
     let newBoard = [];
+
     for (let i = 0; i < board.length; i++) {
         newBoard.push(board[i].join(""));
     }
