a47d75f7ac59c87fb753d52f81874c418e894e9f
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-11 19:19:38 +0530

    Time: 3 ms (60.05%), Space: 57.1 MB (92.57%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function (arr, x) {
+
+    let n = arr.length;
+    let ans = [];
+
+    // left non-overlapping intervals
+    let i = 0;
+    while (i < n && arr[i][1] < x[0]) {
+        ans.push(arr[i]);
+        i++;
+    }
+
+    // overlapping intervals
+    while (i < n && arr[i][0] <= x[1]) {
+        x[0] = Math.min(arr[i][0], x[0]);
+        x[1] = Math.max(arr[i][1], x[1]);
+        i++;
+    }
+
+    ans.push(x);
+
+    // right non-overlapping intervals
+    while (i < n) {
+        ans.push(arr[i]);
+        i++;
+    }
+
+    return ans;
+};
