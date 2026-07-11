c09ad78c75113d0388a1e75fd8558e29c0079d03
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-11 16:32:15 +0530

    Time: 2 ms (50.17%), Space: 54.6 MB (55.32%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -3,20 +3,14 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    let buy = prices[0];
-    let profit = 0;
     let ans = 0;
 
     for (let i = 1; i < prices.length; i++) {
-        if (prices[i] - buy > 0) {
-            profit = prices[i] - buy;
+        let profit = prices[i] - prices[i - 1];
+        if (profit > 0) {
             ans += profit;
-            buy = Infinity;
-        }
-
-        if (prices[i] < buy) {
-            buy = prices[i];
         }
     }
+
     return ans;
 };
