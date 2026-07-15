370f4542a943188bae42ea91a8d35b8e07b60088
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-15 16:45:36 +0530

    Time: 16 ms (8.62%), Space: 60.9 MB (8.11%) - LeetHub

--- 0135-candy/0135-candy.js
+++ 0135-candy/0135-candy.js
@@ -3,40 +3,28 @@
  * @return {number}
  */
 var candy = function (ratings) {
+    let n = ratings.length;
 
-    let candy = 1;
-    let ltr = [1];
-    let rtl = [];
-    rtl[ratings.length - 1] = 1;
-    let ansArr = [];
+    let ltr = Array(n).fill(1);
 
-    for (let i = 0; i < ratings.length - 1; i++) {
-        if (ratings[i + 1] > ratings[i]) {
-            ltr[i + 1] = ++candy;
-        }
-        else {
-            ltr[i + 1] = 1;
-            candy = 1;
+    for (let i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            ltr[i] = ltr[i - 1] + 1;
         }
     }
 
-    candy = 1;
+    let rtl = Array(n).fill(1);
 
-    for (let i = ratings.length - 1; i > 0; i--) {
-        if (ratings[i - 1] > ratings[i]) {
-            rtl[i - 1] = ++candy;
-        }
-        else {
-            rtl[i - 1] = 1;
-            candy = 1;
+    for (let i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            rtl[i] = rtl[i + 1] + 1;
         }
     }
 
     let ans = 0;
 
-    for (let i = 0; i < ratings.length; i++) {
-        ansArr[i] = Math.max(ltr[i], rtl[i]);
-        ans += ansArr[i];
+    for (let i = 0; i < n; i++) {
+        ans = ans + Math.max(ltr[i], rtl[i]);
     }
 
     return ans;
