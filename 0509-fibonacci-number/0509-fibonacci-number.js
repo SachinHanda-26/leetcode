acdeb4d87c8fd15f8e8d6e7f42049546060e49dc
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-16 22:04:03 +0530

    Time: 45 ms (72.46%), Space: 53.9 MB (32.15%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,9 +2,16 @@
  * @param {number} n
  * @return {number}
  */
+
+let store = {};
+
 var fib = function (n) {
 
     if (n <= 1) return n;
 
-    return fib(n - 1) + fib(n - 2);
+    if (!store[n]) {
+        store[n] = fib(n - 1) + fib(n - 2);
+    }
+
+    return store[n];
 };
