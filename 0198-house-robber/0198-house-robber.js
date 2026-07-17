3258f40b8efff7cbd63a821523ee0c74436dc926
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-17 17:34:35 +0530

    Time: 0 ms (100%), Space: 53 MB (82.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -4,11 +4,17 @@
  */
 var rob = function (nums) {
     let n = nums.length;
-    let dp = [nums[0], Math.max(nums[0], nums[1])];
+    if (n == 1) return nums[0];
+    let p2 = nums[0];
+    let p1 = Math.max(nums[0], nums[1]);
 
-    for (let i = 2; i <= n; i++) {
-        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    for (let i = 2; i < n; i++) {
+        let curr = Math.max(nums[i] + p2, p1);
+        let temp = p1;
+        p1 = curr;
+        p2 = temp;
+        ++curr;
     }
 
-    return dp[n - 1];
+    return p1;
 };
