7c18a0550df4f9fd8e31e0d3aa890a04a9578942
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-17 15:53:29 +0530

    Time: 2 ms (55.99%), Space: 57.6 MB (6.57%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -3,19 +3,12 @@
  * @return {number}
  */
 var minCostClimbingStairs = function (cost) {
-    let dp = {};
+    let dp = [0, 0];
+    let n = cost.length;
 
-    function helper(i) {
-        if (i <= 1) {
-            return 0;
-        }
-
-        if (!(i in dp)) {
-            dp[i] = Math.min((helper(i - 1) + cost[i - 1]), (helper(i - 2) + cost[i - 2]));
-        }
-
-        return dp[i];
+    for (let i = 2; i <= cost.length; i++) {
+        dp[i] = Math.min((dp[i - 1] + cost[i - 1]), (dp[i - 2] + cost[i - 2]));
     }
 
-    return helper(cost.length);
+    return dp[n];
 };
