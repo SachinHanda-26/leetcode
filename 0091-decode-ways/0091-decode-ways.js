3f3285e194fe469614b84de0a46a3916a936b5d5
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-21 23:35:19 +0530

    Time: 2 ms (27.55%), Space: 57.3 MB (10.81%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function (s) {
+    let dp = {};
+    function fn(remS) {
+        let n = remS.length;
+        if (remS == '') return 1;
+        if (remS == '0') return 0;
+        if (remS in dp) return dp[remS];
+
+        let oneDigit = remS.substring(n - 1);
+        let twoDigit = remS.substring(n - 2);
+        let ans = 0;
+
+        if (oneDigit != 0) {
+            ans += fn(remS.substring(0, n - 1));
+        }
+
+        if (twoDigit >= 10 && twoDigit <= 26) {
+            ans += fn(remS.substring(0, n - 2));
+        }
+        dp[remS] = ans;
+        return ans;
+    }
+
+    return fn(s);
+};
