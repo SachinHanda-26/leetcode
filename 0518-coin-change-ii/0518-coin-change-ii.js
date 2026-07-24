5b5fdab9b9f02a0e6d6b32a9caf397546cbcef25
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-24 16:37:13 +0530

    Time: 2500 ms (12.95%), Space: 88.4 MB (32.24%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.js
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -4,16 +4,17 @@
  * @return {number}
  */
 var change = function (amount, coins) {
-    let dp = Array.from({ length: amount + 1 }, () => Array(coins.length).fill(null));
+    let n = coins.length;
+    let dp = Array.from({ length: amount + 1 }, () => Array(n).fill(-1));
 
     function fn(remA, start) {
         if (remA == 0) return 1;
         if (remA < 0) return 0;
-        if (dp[remA][start] !== null) return dp[remA][start];
+        if (dp[remA][start] !== -1) return dp[remA][start];
 
         let combinations = 0;
 
-        for (let i = start; i < coins.length; i++) {
+        for (let i = start; i < n; i++) {
             combinations += fn(remA - coins[i], i);
         }
         return dp[remA][start] = combinations;
