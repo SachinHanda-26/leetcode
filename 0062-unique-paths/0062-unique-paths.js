73ce993f07909cd00be2c4bf1c4ee6b820e9613b
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-25 17:11:18 +0530

    Time: 1 ms (57.88%), Space: 52.2 MB (98.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
-    let dp = Array.from({ length: m}, () => Array(n).fill(-1));
+    let dp = Array.from({ length: m }, () => Array(n).fill(-1));
 
-    function fn(x, y) {
-        if (x == 0 && y == 0) return 1;
-        if (x < 0 || y < 0) return 0;
-        if (dp[x][y] != -1) return dp[x][y];
+    for (let i = 0; i < m; i++) { dp[i][0] = 1; }
+    for (let i = 0; i < n; i++) { dp[0][i] = 1; }
 
-        return dp[x][y] = fn(x - 1, y) + fn(x, y - 1);
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
     }
 
-    return fn(m - 1, n - 1);
+    return dp[m - 1][n - 1];
 };
