b2792d0363f6ce5d8060d32427b1ab4cec5b3acb
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-27 00:11:18 +0530

    Time: 55 ms (41.32%), Space: 58.5 MB (9.67%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -22,7 +22,7 @@ var cloneGraph = function (node) {
         for (let n of curr.neighbors) {
             if (!visited.has(n)) {
                 q.push(n);
-                visited.set(n, new Node(n.val))
+                visited.set(n, new Node(n.val));
             }
             let cloneCurr = visited.get(curr);
             cloneCurr.neighbors.push(visited.get(n));
