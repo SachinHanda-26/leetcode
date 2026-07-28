e6c93bcb129b1fa326d54dca6461eb418d82e710
Sachin Handa <sachinhanda8349@gmail.com>
2026-07-28 19:46:54 +0530

    Time: 69 ms (54.28%), Space: 63.6 MB (30.09%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function (people, limit) {
+    people.sort((a, b) => a - b);
+    let start = 0;
+    let end = people.length - 1;
+    let count = 0;
+
+    while (start <= end) {
+        if (people[start] + people[end] <= limit) {
+            start++;
+            end--;
+        } else {
+            end--;
+        }
+        count++;
+    }
+    return count;
+};
