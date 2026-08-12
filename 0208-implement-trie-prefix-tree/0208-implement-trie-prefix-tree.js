018faa6b8cabea8a6e12a3ae76033654f7a24cea
Sachin Handa <sachinhanda8349@gmail.com>
2026-08-12 15:26:42 +0530

    Time: 45 ms (51.47%), Space: 76 MB (41.58%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
@@ -0,0 +1,65 @@
+
+var Trie = function () {
+    this.root = new TrieNode();
+};
+
+var TrieNode = function () {
+    this.children = {};
+    this.isEndOfWord = false;
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+    let curr = this.root;
+    for (let char of word) {
+        if (!curr.children[char]) {
+            curr.children[char] = {
+                children : {},
+                isEndOfWord : false,
+            }
+        }
+        curr = curr.children[char];
+    }
+    curr.isEndOfWord = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (word) {
+    let curr = this.root;
+    for (let char of word) {
+        if (!curr.children[char]) {
+            return false;
+        }
+        curr = curr.children[char];
+    }
+    return curr.isEndOfWord;
+};
+
+/** 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+    let curr = this.root;
+    for (let char of prefix) {
+        if (!curr.children[char]) {
+            return false;
+        }
+        curr = curr.children[char];
+    }
+    return true;
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
