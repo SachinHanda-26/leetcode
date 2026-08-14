d49699f6c148aa4a35e7fe3adec495c6af88a563
Sachin Handa <sachinhanda8349@gmail.com>
2026-08-14 19:55:04 +0530

    Time: 4 ms (34.56%), Space: 57.5 MB (59.34%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+    let sum = 0;
+    let leftSum = 0;
+    
+    for (let num of nums) {
+        sum += num;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        sum = sum - nums[i];
+        if (sum == leftSum) return i;
+        leftSum += nums[i];
+    }
+
+    return -1;
+};
