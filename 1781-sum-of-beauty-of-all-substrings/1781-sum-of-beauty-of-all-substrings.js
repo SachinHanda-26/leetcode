08d266d50ee08bffa3df5101b7be6f97b6271bc7
Sachin Handa <sachinhanda8349@gmail.com>
2026-08-18 20:45:30 +0530

    Time: 213 ms (50%), Space: 57 MB (86.27%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var beautySum = function (s) {
+    let beautySum = 0;
+    let beauty = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let freq = new Array(26).fill(0);
+        for (let j = i; j < s.length; j++) {
+            let index = s.charCodeAt(j) - "a".charCodeAt();
+            freq[index]++;
+
+            let max = 0;
+            let min = Infinity;
+            
+            for (let k = 0; k < 26; k++) {
+                if (freq[k] > 0) {
+                    max = Math.max(max, freq[k]);
+                    min = Math.min(min, freq[k]);
+                }
+            }
+            beautySum += max - min;
+        }
+    }
+    return beautySum;
+};
