3028e610b9fc4b0b5c7e3efc45cb8e0671a4cdcd
Sachin Handa <sachinhanda8349@gmail.com>
2026-08-22 14:46:29 +0530

    Time: 20 ms (20.41%), Space: 61.8 MB (13.47%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.js
+++ 0686-repeated-string-match/0686-repeated-string-match.js
@@ -11,17 +11,55 @@ var repeatedStringMatch = function (a, b) {
         text = text + a;
         count++;
     }
-    if (text.includes(b)) {
+    if (rabinKarp(text, b)) {
         return count;
     }
 
     text = text + a;
     count++;
 
-    if (text.includes(b)) {
+    if (rabinKarp(text, b)) {
         return count;
     }
 
     return -1;
 
+};
+
+function rabinKarp(text, pattern) {
+    let base = 256;
+    let mod = 1e9 + 7;
+
+    let n = text.length;
+    let m = pattern.length;
+
+    let windowHash = 0;
+    let patternHash = 0;
+
+    // Initial Hash of text and pattern
+
+    for (let i = 0; i < m; i++) {
+        patternHash = (patternHash * base + pattern.charCodeAt(i)) % mod;
+        windowHash = (windowHash * base + text.charCodeAt(i)) % mod;
+    }
+
+    // power
+    let power = 1;
+    for (let i = 0; i < m - 1; i++) {
+        power = (power * base) % mod;
+    }
+
+    for (let i = 0; i <= (n - m); i++) {
+        if (windowHash == patternHash) {
+            if (pattern == text.substring(i, i + m)) {
+                return true;
+            }
+        }
+        if (i < n - m) {
+            windowHash = (windowHash - (power * text.charCodeAt(i))) % mod;
+            if (windowHash < 0) windowHash += mod;
+            windowHash = (windowHash * base + text.charCodeAt(i + m)) % mod;
+        }
+    }
+    return false;
 };
