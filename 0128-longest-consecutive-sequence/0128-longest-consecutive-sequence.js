f79db954d04cddc3f66c997bd5d55d39ae60a420
Sachin Handa <sachinhanda8349@gmail.com>
2026-09-04 00:13:32 +0530

    Time: 45 ms (21.05%), Space: 78.7 MB (86.12%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-    if (nums.length == 0) return 0;
-
-    nums.sort((a, b) => a - b);
-
-    let longest = 1;
-    let current = 1;
+    let set = new Set(nums);
+    let longest = 0;
 
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == nums[i + 1]) continue;
+    for (let num of set) {
+        if (!set.has(num - 1)) {
+            let current = num;
+            let currentLength = 1;
 
-        if (nums[i] + 1 == nums[i + 1]) {
-            current++;
-        } else {
-            longest = Math.max(longest, current);
-            current = 1;
+            while (set.has(current + 1)) {
+                current++;
+                currentLength++;
+            }
+
+            longest = Math.max(longest, currentLength);
         }
     }
+
     return longest;
 };
