a7429fd438d6812c8df8dc3282ba037e5e096881
Sachin Handa <sachinhanda8349@gmail.com>
2026-09-11 12:17:26 +0530

    Time: 2 ms (90.48%), Space: 58.3 MB (35.71%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.js
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.js
@@ -14,29 +14,39 @@ var totalNumbers = function (digits) {
     for (let i = 1; i <= 9; i++) {
         if (count[i] === 0) continue;
 
+        // use i - means digit
         count[i]--;
 
         for (let j = 0; j <= 9; j++) {
             if (count[j] == 0) continue;
+
+            // use j
             count[j]--;
 
             for (let k = 0; k <= 9; k++) {
                 if (count[k] == 0) continue;
+
+                // checks that the last digit should be even for even num
                 if (k % 2 == 0) {
+
+                    // use k
                     count[k]--;
 
+                    // generate a num
                     let num = 100 * i + 10 * j + k;
 
                     if (!set.has(num)) {
                         set.add(num);
                         ans++;
                     }
+                    // restore k
                     count[k]++;
                 }
             }
+            // restore j
             count[j]++
         }
-
+        // restore i
         count[i]++;
     }
     return ans;
