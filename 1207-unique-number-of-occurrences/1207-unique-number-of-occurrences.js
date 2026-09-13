63b46088573981cf8b99ada8a28d885300a6219c
Sachin Handa <sachinhanda8349@gmail.com>
2026-09-13 23:02:41 +0530

    Time: 1 ms (64.12%), Space: 54 MB (66.07%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+    let map = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!map[arr[i]]) map[arr[i]] = 1;
+        else map[arr[i]]++;
+    }
+
+    values = Object.values(map);
+
+    let set = new Set(values);
+
+    return values.length === set.size;
+};
